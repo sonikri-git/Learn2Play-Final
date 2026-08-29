bb79cd1a7767ddf82a85a7f762ba78e8cc38a363
Kritish Soni <Kritishsoni2004@gmail.com>
2026-08-29 19:54:42 -0400

    Fix duplicate React import

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/global.scss';
-createRoot(document.getElementById('root')).render(<React.StrictMode><BrowserRouter><App/></BrowserRouter></React.StrictMode>);
-// Production React import fix
-import React from 'react';
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
